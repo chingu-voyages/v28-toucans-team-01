9cc2ab8327f5e11244ec7c61bedf13aa5159d147
Garman_K <garnetbirthstone@hotmail.com>
2021-03-10 18:26:47 -0500

    brainstorming animals

---
+++ questions.js
@@ -0,0 +1,166 @@
+// Questions for the quiz app
+
+/* list of animals: 
+
+    reptiles: snake, turtle, alligator, lizard
+    amphibians: frog, toad, newt, salamander
+    mammals: wolf, elephant, horse, seal, monkey, shark, bear, kangaroo
+    birds: penguin, eagle, flamingo, peacock, owl, ostrich, swan 
+
+*/
+
+// array of questions and answers
+
+const myQuestions = [
+    {
+        question: `What are you most likely to eat after a stressful day?`,
+        answers:
+        {
+            a: `Popcorn or chips`,
+            b: `A crisp salad`,
+            c: `Everything in sight`,
+            d: `Something sweet`
+        }
+    },
+    {
+        question: `What is your favorite way to travel?`,
+        answers:
+        {
+            a: `Boat`,
+            b: `Walk`,
+            c: `Car`,
+            d: `Fly`
+        }
+    },
+    {
+        question: `When you have something exciting to share, how do you tell people?`,
+        answers:
+        {
+            a: `I jump around`,
+            b: `I keep it to myself and reveal the news when everyone is least expecting it`,
+            c: `Grab the entire herd's attention`,
+            d: `I spread the news like crop disease`
+        }
+    },
+    {
+        question: `Who is your ideal mentor?`,
+        answers:
+        {
+            a: `Kermit the Frog`,
+            b: `Mark Zuckerberg`,
+            c: `Professor Dumbledore`,
+            d: `Beyonce`
+        }
+    },
+    {
+        question: `How would you describe your perfect home?`,
+        answers:
+        {
+            a: `Waterfront property`,
+            b: `Very tidy and warm`,
+            c: `Cozy`,
+            d: `Kind of messy with a good view`
+        }
+    },
+    {
+        question: `Which is your favourite way to travel?`,
+        answers:
+        {
+            a: `Airplane`,
+            b: `Car`,
+            c: `Boat`,
+            d: `Bicycle or on foot`
+        }
+    },
+    {
+        question: `What's your favorite season of the year'?`,
+        answers:
+        {
+            a: `Spring`,
+            b: `Summer`,
+            c: `Winter`,
+            d: `Something sweet`
+        }
+    },
+    {
+        question: `What are you most likely to eat after a stressful day?`
+        answers:
+        {
+            a: `Popcorn or chips`,
+            b: `A crisp salad`,
+            c: `Everything in sight`,
+            d: `Something sweet`
+        }
+    },
+    {
+        question: `What are you most likely to eat after a stressful day?`
+        answers:
+        {
+            a: `Popcorn or chips`,
+            b: `A crisp salad`,
+            c: `Everything in sight`,
+            d: `Something sweet`
+        }
+    },
+    {
+        question: `What are you most likely to eat after a stressful day?`
+        answers:
+        {
+            a: `Popcorn or chips`,
+            b: `A crisp salad`,
+            c: `Everything in sight`,
+            d: `Something sweet`
+        }
+    },
+    {
+        question: `What are you most likely to eat after a stressful day?`
+        answers:
+        {
+            a: `Popcorn or chips`,
+            b: `A crisp salad`,
+            c: `Everything in sight`,
+            d: `Something sweet`
+        }
+    },
+    {
+        question: `What are you most likely to eat after a stressful day?`
+        answers:
+        {
+            a: `Popcorn or chips`,
+            b: `A crisp salad`,
+            c: `Everything in sight`,
+            d: `Something sweet`
+        }
+    },
+    {
+        question: `What are you most likely to eat after a stressful day?`
+        answers:
+        {
+            a: `Popcorn or chips`,
+            b: `A crisp salad`,
+            c: `Everything in sight`,
+            d: `Something sweet`
+        }
+    },
+    {
+        question: `What are you most likely to eat after a stressful day?`
+        answers:
+        {
+            a: `Popcorn or chips`,
+            b: `A crisp salad`,
+            c: `Everything in sight`,
+            d: `Something sweet`
+        }
+    },
+    {
+        question: `What are you most likely to eat after a stressful day?`
+        answers:
+        {
+            a: `Popcorn or chips`,
+            b: `A crisp salad`,
+            c: `Everything in sight`,
+            d: `Something sweet`
+        }
+    }
+
+]
